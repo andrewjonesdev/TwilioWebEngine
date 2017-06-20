9df6c06c3e74a6bdf34e8724179e1e76b00db5ca
Andrew Jones <jones.gaffney1920@gmail.com>
2017-06-20 09:16:15 -0400

    some changes

--- src/com/twilio/TwilioServlet.java
+++ src/com/twilio/TwilioServlet.java
@@ -1,34 +1,33 @@
 package com.twilio;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 import com.twilio.twiml.Body;
 import com.twilio.twiml.Message;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.TwiMLException;
+//@WebServlet("/sms")
+
 
 public class TwilioServlet extends HttpServlet {
-
+	MessagingResponse twiml;
     // service() responds to both GET and POST requests.
     // You can also use doGet() or doPost()
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-       /* Message message = new Message.Builder()
-                .body(new Body("Hello, Mobile Monkey"))
-                .build();
-
-        MessagingResponse twiml = new MessagingResponse.Builder()
-                .message(message)
-                .build();
-
-        response.setContentType("application/xml");
-
-        try {
-            response.getWriter().print(twiml.toXml());
-        } catch (TwiMLException e) {
-            e.printStackTrace();
-        }*/
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+	}
+	
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+EngineStart.execute();
+       
     }
 }
